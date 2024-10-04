ee31880d4fd00a462bf8bb6804f32d750ea64862
yanshiii <144547830+yanshiii@users.noreply.github.com>
2024-10-04 20:54:59 +0530

    fixing search issue 1

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const API_KEY = 'fadfa28f7146e830e8851ea0361e637b';
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast';
-const GEOCODING_API_URL = 'https://api.openweathermap.org/geo/1.0/direct';
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -105,18 +104,3 @@ getLocation();
 
 
 
-
-const locationSearch = document.getElementById('location-search');
-const locationSuggestions = document.getElementById('location-suggestions');
-
-locationSearch.addEventListener('input', debounce(searchLocations, 300));
-
-function debounce(func, delay) {
-    let timeout;
-    return function(...args) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            func.apply(this, args);
-        }, delay);
-    };
-}
